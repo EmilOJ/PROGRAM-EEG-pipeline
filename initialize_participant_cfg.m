66de29e54cc82035c66d49f0228fc47b23001d4c
DRCMR User <emilj@big27.drcmr.ext>
2016-03-18 10:40:14 +0100

    Fix filepath bug

--- initialize_participant_cfg.m
+++ initialize_participant_cfg.m
@@ -4,6 +4,6 @@ function [cfg] = initialize_participant_cfg(experiment, participant)
     cfg.rootdir                 = [root filesep];
     cfg.subjectnr               = num2str(participant);
     cfg.subjectstr              = ['part' cfg.subjectnr];
-    cfg.dataset                 = [cfg.subjectstr '.bdf'];
     cfg.datadir                 = [cfg.rootdir 'EEG-' experiment filesep];
+    cfg.dataset                 = [cfg.datadir cfg.subjectstr '.bdf'];
 end
